e01c593ec1ee95a03339e2c9cbb1e16129240896
Jeffrey van Helden <311759@twgroup.co.nz>
2021-06-25 15:01:57 +1200

    topic configurable in docker run command

--- docker/start-kafka.sh
+++ docker/start-kafka.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 sed -e "s|{CONNECTION}|$ENV_CONNECTION|g" -e "s|{USERNAME}|$ENV_USERNAME|g" -e "s|{PASSWORD}|$ENV_PASSWORD|g" /opt/kafka/config/connect-es-sink.properties > /opt/kafka/config/connect-es-sink-runtime.properties
-sed -e "s|{BROKER}|$ENV_BROKER|g" -e "s|{KEY}|$ENV_KEY|g" -e "s|{SECRET}|$ENV_SECRET|g" /opt/kafka/config/connect-standalone.properties > /opt/kafka/config/connect-standalone-runtime.properties
-echo "--------------------------------------------------"
-echo "show me the money!"
-echo "--------------------------------------------------"
-echo $ENV_BROKER
-echo $ENV_KEY
-echo $ENV_SECRET
-echo "--------------------------------------------------"
-cat /opt/kafka/config/connect-standalone-runtime.properties
+
+if [ ! -z "$ENV_TOPIC" ]; then
+    echo $ENV_TOPIC
+    sed -i -e "s/confluent-audit-log-events/$ENV_TOPIC/g" /opt/kafka/config/connect-es-sink-runtime.properties
+fi
+
+echo "show me the connector config!"
 echo "--------------------------------------------------"
 cat /opt/kafka/config/connect-es-sink-runtime.properties
+echo "--------------------------------------------------"
+
+sed -e "s|{BROKER}|$ENV_BROKER|g" -e "s|{KEY}|$ENV_KEY|g" -e "s|{SECRET}|$ENV_SECRET|g" /opt/kafka/config/connect-standalone.properties > /opt/kafka/config/connect-standalone-runtime.properties
+echo "--------------------------------------------------"
+echo "show me the connect cluster config!"
+cat /opt/kafka/config/connect-standalone-runtime.properties
 echo "--------------------------------------------------"
 
 # connector start command here.
